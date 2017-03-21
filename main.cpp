060f5335de1e8706b29e507881d0f1f0d1504452
kotha <kotha96itlike@gmail.com>
2017-03-21 17:12:51 +0700

    lab5.3

---
+++ main.cpp
@@ -0,0 +1,8 @@
+//This programm about tragic love (lab3 5.3 Belykov)
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main(int argc, char const *argv[]) {
+  float i;
+  return 0;
+}
