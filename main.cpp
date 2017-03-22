df3594c912c15afe06c05e9b6687d4c44b9d0ca0
kotha96itlike@gmail.com <kotha96itlike@gmail.com>
2017-03-22 22:32:55 +0700

    fixing bugs

--- main.cpp
+++ main.cpp
@@ -1,9 +1,28 @@
 //This programm about tragic love (lab3 5.3 Belykov)
 #include <iostream>
-#include <cmath>
 using namespace std;
-int main(int argc, char const *argv[]) {
-  float i, n;
-  cin >> n;
-  return 0;
+int proiz(int n, int p, int *arr);
+
+int main(int argc, char *argv[])
+{
+    int n,p;
+    cin >> n; //rand ()%RAND_MAX
+    int *arr = new int[n];
+
+    p=proiz( n, p, arr);
+    cout << p;
+    return 0;
+}
+
+int proiz(int n, int p, int *arr)
+{
+    for (int i=0; i < n; i++)
+    {
+        if ((i%2)==0)
+        {
+        p*=arr[i];
+        }
+    }
+
+    return p;
 }
