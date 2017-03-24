9baedaa96e5f810253d059724c3e01133e02133a
kotha96itlike@gmail.com <kotha96itlike@gmail.com>
2017-03-24 20:44:01 +0700

    watthefck

--- main.cpp
+++ main.cpp
@@ -1,9 +1,37 @@
-#include <QCoreApplication>
+//This programm about tragic love (lab3 5.3 Belykov)
+// 50%
+#include <iostream>
+using namespace std;
 
 int main(int argc, char *argv[])
 {
-    QCoreApplication a(argc, argv);
+    int n,proiz=1,summ=0;
+    cout << "n= ";
+    cin >> n;
+    int *arr = new int[n];
 
-    return a.exec();
+    for (int i=0; i < n; i++)
+    {
+        cout << i << " = ";
+        cin >> arr[i]; //ввод в массив
+    }
+
+    for (int i=0; i < n; i++)
+    {
+        if ((i%2)==0)
+        {
+            proiz*=arr[i];
+        }
+    }
+    for (int i=0; i < n; i++)
+    {
+        if (arr[i]!=0)
+        {
+        summ+=arr[i];
+        }
+    }
+
+    cout << "summ= " << summ << endl;
+    cout << "proiz= " << proiz << endl;
+    return 0;
 }
-
