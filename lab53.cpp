f3ab20110adf05d7906bd82db964e2be95a96e36
kotha96itlike@gmail.com <kotha96itlike@gmail.com>
2017-04-01 02:10:18 +0700

    5.3 is done by me and crusaderteam

---
+++ lab53.cpp
@@ -0,0 +1,47 @@
+﻿//lab3 5.3 Belykov
+// 100%
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int n,proiz=1,summ=0,j=0,k=0,por=0;
+    cout << "n= ";
+    cin >> n;
+    int *arr = new int[n];
+
+    for (int i=0; i < n; i++)
+    {
+        cout << i << " = ";
+        cin >> arr[i];
+    }
+//произведение эл. с четными номерами
+    for (int i=0; i < n; i++)
+        if ((i%2)==0) proiz*=arr[i];
+//сумма эл. между 1 и последним нулевым значением
+    for(int i=0;i<n;i++)
+          if(arr[i]==0)
+          {
+              j=i; break;
+          }
+    for(int i=0;i<n;i++)
+          if(arr[i]==0)
+          k=i;
+    for(int i=j;i<k;i++)
+          summ+=arr[i];
+//сортировка массива от большего к меньшему
+    for (int i=0; i<n; i++)
+    for (int j=0; j<n; j++)
+        if ((arr[i]>arr[j])&&(i!=j))
+    {
+        arr[i]=arr[i]+arr[j];
+        arr[j]=arr[i]-arr[j];
+        arr[i]=arr[i]-arr[j];
+    }
+    for (int i=0;i<n;i++)
+    cout<<arr[i]<<" ";
+    
+    cout << endl <<"summ= " << summ << endl;
+    cout << "proiz= " << proiz;
+    return 0;
+}
