5df2b6d257f42bda621747c7bed435fc06b76b9d
kotha96itlike@gmail.com <kotha96itlike@gmail.com>
2017-04-01 11:20:01 +0700

    6.3 DONE

--- lab63.cpp
+++ lab63.cpp
@@ -1,14 +1,36 @@
 ﻿//lab3 6.3 Belykov
-// 0%
+// 100%
+/* for VS
+#include "stdafx.h" = <cstdlib>
+*/
 #include <iostream>
+#include <ctime>
+#include <iomanip>
+#include <cstdlib>
 using namespace std;
 
 int main()
 {
-    int n;
-    cout << "n= ";
-    cin >> n;
-    int *arr = new int[n];
+    int i,j,s=1;
+    //задание массива
+    int m[5][5];
+    int b[5];
+    srand(time(0));
+    for(j=0;j<5;j++)
+    {
+    for(i=0;i<5;i++)
+        {
+          m[j][i]=rand()%3+1;
+        }
+    }
+    for (j=0;j<5;j++)
+    {
+    for (i=0;i<5;i++)
+    {
+        s*=m[j][i];
+    }
+    b[j]=s; s=1; cout << b[j] << endl;
+    }
 
     return 0;
 }
