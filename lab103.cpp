cc9c7c8ee54265774923765e55b4783926efafdb
kothha <kotha96itlike@gmail.com>
2017-05-02 20:17:16 +0700

    updated but doesnt work

--- lab103.cpp
+++ lab103.cpp
@@ -2,21 +2,28 @@
 #include <iomanip>
 using namespace std;
 
+struct ycheniki {
+    char fam[15];
+    char name[15];
+    char bykva;
+    int nomer;
+};
+
 int main()
 {
-    struct ycheniki {
-        char fam[15];
-        char name[15];
-        char bykva;
-        int nomer;
-    };
-    ycheniki spis[10]; int n=10;
-    for (i=0; i<=n; i++) {
-        cout << "Famulu9, um9 , bykBa ,Homep"; //lol
-        cin >> spis[i].fam >> spis[i].name >> spis[i].bykva >> spis[i].nomer;
+    setlocale(LC_ALL, "Russian");
+    ycheniki spis[11]; int n=2; int i,k;
+    for (i=1; i<=n; i++) {
+        cout << "Фамилия, имя, номер и буква ученика " << i << endl; //lol
+        cin >> spis[i].fam >> spis[i].name >> spis[i].nomer >> spis[i].bykva;
     }
-
+    cout << "Одиноковое имя и фамилия у: ";
+    for (i=1; i<=n-1; i++) {
+    for (k=i+1; k<=n; k++) {
+        if ((spis[i].fam==spis[k].fam)&&(spis[i].name==spis[k].name)) {
+            cout << spis[i].fam << " " << spis[i].name << " " << spis[i].nomer << " " << spis[i].bykva << endl;
+            cout << spis[k].fam << " " << spis[k].name << " " << spis[k].nomer << " " << spis[k].bykva << endl;
+        } } }
     return 0;
 }
-
 
