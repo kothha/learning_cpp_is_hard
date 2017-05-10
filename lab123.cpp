ea2ed5b23c643246853a43a1122372b0b844d4e0
kothha <kotha96itlike@gmail.com>
2017-05-10 15:11:19 +0700

    2% ready

---
+++ lab123.cpp
@@ -0,0 +1,19 @@
+#include "stdafx.h"
+#include <iostream>
+#include <cmath>
+using namespace std;
+class EngMoney
+{
+public:
+	int fynt;
+	int shilingov;
+	int pensov;
+	void vyvod();
+	void vvod();
+};
+
+int main()
+{
+	system("pause");
+    return 0;
+}
